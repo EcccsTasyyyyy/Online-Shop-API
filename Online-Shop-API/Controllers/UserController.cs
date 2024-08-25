8ac6afef0b7cdf738d9516ada66eea60ae365db6
EcccsTasyyyyy <lashatutberidze1@gmail.com>
2024-08-25 20:22:42 +0400

    2

--- Online-Shop-API/Controllers/UserController.cs
+++ Online-Shop-API/Controllers/UserController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Online_Shop_API.Controllers;
 
+[Authorize(Roles = "User")]
 [Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
 {
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("You have accessed the User controller.");
+    }
 }
